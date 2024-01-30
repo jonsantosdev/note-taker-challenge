4333283c22c3e50d9515dd5b7ed0718f5a1c2958
jonsantosdev <js.jsantos.jr@gmail.com>
2024-01-30 14:03:05 -0800

    added environment port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const uuid = require('./helpers/uuid');
 
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
